502278827bd3ff8e8d78a23c53bdf2fcfbfa575a
lena-vllz <lenavulliez@gmail.com>
2021-03-25 22:29:29 +0100

    mise en page pop up

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,14 +4,18 @@ function Popup({selected, closePopup}) {
     return (
         <section className="popup">
             <div className="content">
-                <h2>{ selected.Title } <span>({selected.Year})</span></h2>
+                <h2>{ selected.Title }</h2>
                 <p className="rating"> Rating: {selected.imdbRating}/10</p>
-                <p className="author"> Actors: {selected.Actors}</p>
-                <div className="plot">
-                    <img src={selected.Poster} />
-                    <p>{selected.Plot}</p>
+                <div className="contentFlex">
+                    <img class="affiche_film" src={selected.Poster} />
+                    <div className="info_content">
+                       <p className="director"> Director: {selected.Director}</p>
+                        <p className="year">{selected.Year}</p>
+                        <p className="author"> Actors: {selected.Actors}</p>
+                        <p className="plot">{selected.Plot}</p> 
+                    </div>
                 </div>
-                <button clasName="close" onClick={closePopup}>Close</button>
+                <button className="close" onClick={closePopup}>Close</button>
             </div>
         </section>
     )
