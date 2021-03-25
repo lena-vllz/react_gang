0aac5e5a55e3c7aeb9cac0e79b055e4a8666ce42
lena-vllz <lenavulliez@gmail.com>
2021-03-25 01:00:32 +0100

    pop up (broke)

--- formation-react/src/App.js
+++ formation-react/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import Search from './components/Search'
 import Results from './components/Results'
+import Popup from './components/Popup'
 
 function App() {
   const [state, setState] = useState({
@@ -30,9 +31,26 @@ function App() {
     setState(prevState => {
       return { ...prevState, s: s}
     });
-
-    
+ 
   }
+
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({data})=>{
+      let result = data;
+
+      setState(prevState => {
+        return {...prevState, selected: result}
+      });
+    });
+
+  }
+
+  const closePopup = () => {
+    setState(prevState =>{
+      return{...prevState, selected:{}}
+    });
+  }
+
 
   return (
     <div className="App">
@@ -41,7 +59,8 @@ function App() {
       </header>
       <main>
         <Search handleInput={handleInput} search={search}/>
-        <Results results={state.results} />
+        <Results results={state.results} openPopup={openPopup} />
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup= {closePopup} /> : false}
       </main>
     </div>
   );
