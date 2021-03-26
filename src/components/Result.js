609c897c4f14d9e3172ce4595128f4fa56a618c6
lena-vllz <lenavulliez@gmail.com>
2021-03-26 12:19:16 +0100

    comment

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-
+// function that returns the elements you want when you do a search
 function Result({result, openPopup}) {
     return (
+        //on click in a result use the openPopup function
         <div className="result" onClick={() => openPopup(result.imdbID)}>
             <img src={result.Poster}/>
             <div className="hoover_div">
                 <div className="hoover_organise">
+                    {/* get the title, the year and the rating of the movie */}
                     <h3 className="hover_title">{result.Title}</h3>
                     <p className="hover_year">{result.Year}</p>
                     <p className="hover_rating">{result.imdbRating}/10</p>
