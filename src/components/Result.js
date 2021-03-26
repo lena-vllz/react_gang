42b579f128489961db0153dec217073058a1484f
Chompy0067 <philippe.eggerl@hetic.net>
2021-03-26 12:55:14 +0100

    Image for non api :nut_and_bolt:
    Tested image for non api token

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 // function that returns the elements you want when you do a search
 function Result({result, openPopup}) {
+    
+    {/* if (result.Poster == true){
+                <img src={result.Poster}/>
+            } else {
+                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5yDB2rWsDM3n8DjnbyiGWVmr0w8CD9voLkH9vla9FiRzI8E3o72JT9lw1LGB96AkYWds&usqp=CAU"}/>
+            } */}
+    
     return (
         //on click in a result use the openPopup function
         <div className="result" onClick={() => openPopup(result.imdbID)}>
+            
             <img src={result.Poster}/>
             <div className="hoover_div">
                 <div className="hoover_organise">
-                    {/* get the title, the year and the rating of the movie */}
+                    {/* get the title, the year and the rating of the movie on hover */}
                     <h3 className="hover_title">{result.Title}</h3>
                     <p className="hover_year">{result.Year}</p>
                     <p className="hover_rating">{result.imdbRating}/5</p>
