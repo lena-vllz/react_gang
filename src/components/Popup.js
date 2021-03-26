330db9791f9189c797a0911d126cbd8f81848611
Chompy0067 <philippe.eggerl@hetic.net>
2021-03-26 10:37:17 +0100

    Stars :star:
    Added star and half stars

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,20 +5,86 @@ import '../index.css';
 import Moon from './assets/moon.png';
 import Sun from './assets/sun.png';
 
-function Popup({selected, closePopup}) {
 
-    for (var i = 0; i < Math.round(selected.imdbRating/2); i++) {
-        console.log("🟊");
+
+function Popup({selected, closePopup}) {
+    const starFull = "https://img.icons8.com/fluent/452/star-filled.png"
+    const starHalf = "https://img.icons8.com/material-outlined/452/star-half.png"
+    const starEmpty = "https://www.my-etiquettes.com/wp-content/uploads/2017/10/Bordure-etoile-jaune.svg"
+    let state1 = starEmpty
+    let state2 = starEmpty
+    let state3 = starEmpty
+    let state4 = starEmpty
+    let state5 = starEmpty
+    let table2 = []
+    const rate = Math.round((selected.imdbRating/2)*2)/2
+    
+
+    let nbStar = 0
+    let tableStar = []
+
+    for (var i = 0; i < rate; i++) {
+        nbStar ++
         document.getElementById("test")
-        document.images.src= "https://lh3.googleusercontent.com/proxy/FBeOdPupu3vhu9EyDFnEpYJRqVW_lJ7OjIkycLVmlEwgQyP4FDk6tVLiepHq7iKmQYzC9_pwcyYsEfD1dChqbBM1rA2hhDDY"
+        tableStar.push("🟊")
     }
+
+    for(let i = 0.5; i<=5; i+=0.5){
+            if(i<=rate){
+                table2.push(i)
+                console.log(table2)
+            }
+        }
+     if(table2.includes(0.5)){
+        state1 = starHalf
+        if(table2.includes(1)){
+            state1 = starFull
+            if(table2.includes(1.5)){
+                state2 = starHalf
+                if(table2.includes(2)){
+                    state2 = starFull
+                    if(table2.includes(2.5)){
+                        state3 = starHalf
+                        if(table2.includes(3)){
+                            state3 = starFull
+                            if(table2.includes(3.5)){
+                                state4 = starHalf
+                                if(table2.includes(4)){
+                                    state4 = starFull
+                                    if(table2.includes(4.5)){
+                                        state5 = starHalf
+                                        if(table2.includes(5)){
+                                            state5 = starFull
+                                         }
+                                     }
+                                 }
+                             }
+                         }
+                     }
+                 }
+                 
+             }
+         }
+     }
+     
+     
 
     return (
         <section className="popup">
             <Header/>
             <div className="content">
                 <h2 className="title">{ selected.Title }</h2>
-                <p className="rating"> Rating: {selected.imdbRating}/10</p>
+                <p className="rating"> Rating: {rate}/10 </p>
+                <p className="rating"> Rating: {tableStar}/5</p>
+
+                <div className="rating"> 
+                    <img src = {state1} alt="rating"></img>
+                    <img src = {state2} alt="rating"></img>
+                    <img src = {state3} alt="rating"></img>
+                    <img src = {state4} alt="rating"></img>
+                    <img src = {state5} alt="rating"></img>
+                </div>
+
                 <div className="contentFlex">
                     <img class="affiche_film" src={selected.Poster} />
                     <div className="info_content">
