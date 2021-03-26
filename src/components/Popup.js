720ce6563f189d5692dd880be17842f0cc861db1
alexandrabernard <60607070+alexandrabernard@users.noreply.github.com>
2021-03-26 05:00:05 +0100

    crea footer :)

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -17,20 +17,19 @@ function Popup({selected, closePopup}) {
         <section className="popup">
             <Header/>
             <div className="content">
-                <h2>{ selected.Title }</h2>
+                <h2 className="title">{ selected.Title }</h2>
                 <p className="rating"> Rating: {selected.imdbRating}/10</p>
                 <div className="contentFlex">
                     <img class="affiche_film" src={selected.Poster} />
                     <div className="info_content">
                        <p className="director"> Director: {selected.Director}</p>
-                        <p className="year">{selected.Year}</p>
+                        <p className="year">Released on {selected.Year}</p>
                         <p className="author"> Actors: {selected.Actors}</p>
                         <p className="plot">{selected.Plot}</p>
                     </div>
                 </div>
                 <button className="close" onClick={closePopup}>Close</button>
             </div>
-            <button className="close" onClick={closePopup}>Close</button>
         </section>
     )
 };
